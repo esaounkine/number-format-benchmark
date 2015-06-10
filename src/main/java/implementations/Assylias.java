49e268e0d88ad30878322bcb489443a4b9b0577c
Elijah Saounkine <eli@datamoil.com>
2015-06-10 13:36:03 +0300

    added another implementation for assylias

--- src/main/java/implementations/Assylias.java
+++ src/main/java/implementations/Assylias.java
@@ -1,55 +1,36 @@
 package implementations;
 
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 /**
  * @author assylias
  */
 public class Assylias {
 
-    private static final String[] suffixes = {"", "k", "m", "b", "t"};
-
-    /**
-     * Returns the value formatted using the following rules:
-     * - if value is negative, apply the rules below to the positive number and prefix with -
-     * - if value is between 0 and 999 returns as is
-     * - otherwise:
-     *    - the value is rescaled to be printed with the appropriate suffix
-     *      (k for thousands, m for millions, b for billions and t for trillions)
-     *    - the rescaled value is truncated to one decimal place if it's less than 10
-     *      or to an integer otherwise
-     *
-     * Examples:
-     * 1         -> "1"
-     * 999       -> "999"
-     * 1,000     -> 1k
-     * 1,999     -> 1.9k
-     * 10,000    -> 10k
-     * 999,999   -> 999k
-     * 1,000,000 -> 1m
-     * -1,230    -> -1.2k
-     */
-    public static String format(long value) {
-        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1); //to prevent problem on next line
-        long positiveValue = Math.abs(value); //works ok because value != MIN_VALUE
-        long temp = positiveValue;
-        int suffixIndex = 0;
-        while ((temp /= 1000) > 0 && suffixIndex < suffixes.length - 1) suffixIndex++;
-        double truncatedValue = truncate(positiveValue, suffixIndex);
-        String suffix = suffixes[suffixIndex];
-        boolean hasDecimal = truncatedValue != (long) truncatedValue;
-        String positiveFormatted = hasDecimal ? truncatedValue + suffix : (long) truncatedValue + suffix;
-        return (value < 0 ? "-" : "") + positiveFormatted;
+    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    static {
+        suffixes.put(1_000L, "k");
+        suffixes.put(1_000_000L, "M");
+        suffixes.put(1_000_000_000L, "G");
+        suffixes.put(1_000_000_000_000L, "T");
+        suffixes.put(1_000_000_000_000_000L, "P");
+        suffixes.put(1_000_000_000_000_000_000L, "E");
     }
 
-    /**
-     * @return the number part of the output, truncated as required with the correct number of decimals
-     */
-    private static double truncate(long positiveValue, int range) {
-        if (range == 0) return positiveValue;
-        long divideBy = 100; //not 1000 to keep one decimal if needed
-        for (int i = 0; i < range - 1; i++) divideBy *= 1000;
-        //cast as double after division to avoid loss of precision!!
-        double scaled = (positiveValue / divideBy) / 10d; //1000 for k, 1_000_000 for m etc.
-        boolean hasDecimal = scaled < 10 && scaled != (long) scaled;
-        return hasDecimal ? (long) (scaled * 10) / 10d : (long) scaled;
+    public static String format(long value) {
+        //Long.MIN_VALUE == -Long.MIN_VALUE so we need a dirty adjustment
+        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+        if (value < 0) return "-" + format(-value);
+        if (value < 1000) return Long.toString(value); //deal with easy case
+
+        Map.Entry<Long, String> e = suffixes.floorEntry(value);
+        Long divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        long truncated = value / (divideBy / 10); //the number part of the output times 10
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
     }
 }
